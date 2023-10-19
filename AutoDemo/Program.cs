234ee760c91329acc54a8b127f6bd811b1a1d4c0
Andrii Podkolzin <andrii.podkolzin@namecheap.com>
2023-10-19 13:20:36 +0300

    increment

--- AutoDemo/Program.cs
+++ AutoDemo/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/version", () => new { version = 4 });
+app.MapGet("/version", () => new { version = 5 });
 
 app.Run();
 
